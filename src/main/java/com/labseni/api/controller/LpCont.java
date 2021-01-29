87f1003a74cd636ef158656b3f742b1bdff8021b
Aziz Faozi <admin@labseni.com>
2021-01-29 13:39:56 +0700

    mantap

--- src/main/java/com/labseni/api/controller/LpCont.java
+++ src/main/java/com/labseni/api/controller/LpCont.java
@@ -3,14 +3,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.util.Date;
 @Controller
 public class LpCont {
 
+	private String appMode;
 	@RequestMapping("/")
-	public String index() {
-		return "index";
-	
-	}
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "labseni");
+       	model.addAttribute("mode", appMode);
+
+        return "index";
+    }
 
 	@GetMapping("/test")
 	public String test(){
